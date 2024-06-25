e862f41251686ed82211887185430f3585e99fa7
Arthur Zopellaro <github@zope.me>
2024-06-25 14:59:59 -0300

    feat: copy nvim-cmp config from kickstart

--- lua/plugins/nvim_cmp.lua
+++ lua/plugins/nvim_cmp.lua
@@ -1,80 +1,77 @@
-vim.g.UltiSnipsExpandTrigger = '<Plug>(ultisnips_expand)'
-vim.g.UltiSnipsJumpForwardTrigger = '<Plug>(ultisnips_jump_forward)'
-vim.g.UltiSnipsJumpBackwardTrigger = '<Plug>(ultisnips_jump_backward)'
-vim.g.UltiSnipsListSnippets = '<c-q>'
-vim.g.UltiSnipsJumpForwardTrigger="<c-b>"
--- vim.g.UltiSnipsJumpForwardTrigger="<c-n>"
--- vim.g.UltiSnipsJumpBackwardTrigger="<c-p>"
-vim.g.UltiSnipsJumpBackwardTrigger="<c-z>"
-vim.g.UltiSnipsRemoveSelectModeMappings = 0
-
-
+-- config from nvim-lua/kickstart.nvim
 return {
-	"hrsh7th/nvim-cmp",
+	'hrsh7th/nvim-cmp',
+	event = 'InsertEnter',
 	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"onsails/lspkind.nvim", -- icons
-		-- snippet
-		"SirVer/ultisnips",
-		"quangnguyen30192/cmp-nvim-ultisnips",
-		"honza/vim-snippets",
-		-- "L3MON4D3/LuaSnip",
-		-- "saadparwaiz1/cmp_luasnip",
-		-- "hrsh7th/cmp-vsnip",
-		-- "hrsh7th/vim-vsnip",
+		-- Snippet Engine & its associated nvim-cmp source
+		{
+			'L3MON4D3/LuaSnip',
+			build = (function()
+				-- Remove the below condition to re-enable on windows.
+				if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+					return
+				end
+				return 'make install_jsregexp'
+			end)(),
+			dependencies = {
+				{
+					'rafamadriz/friendly-snippets',
+					config = function()
+						require('luasnip.loaders.from_vscode').lazy_load()
+					end,
+				},
+			},
+		},
+		'saadparwaiz1/cmp_luasnip',
+		"hrsh7th/cmp-nvim-lua",
+		'hrsh7th/cmp-nvim-lsp',
+		"hrsh7th/cmp-buffer",
+		'hrsh7th/cmp-path',
 	},
 	config = function()
-		local cmp = require("cmp")
-		local lspkind = require("lspkind")
-		cmp.setup({
-			formatting = { format = lspkind.cmp_format() },
-			mapping = cmp.mapping.preset.insert({
-				-- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				-- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-o>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
-				-- ["Up"] = cmp.config.disable,  -- not working
-				-- ["Down"] = cmp.config.disable,
-			}),
+		-- See `:help cmp`
+		local cmp = require 'cmp'
+		local luasnip = require 'luasnip'
+		luasnip.config.setup {}
+
+		cmp.setup {
 			snippet = {
 				expand = function(args)
-					vim.fn["UltiSnips#Anon"](args.body)
-					-- require('luasnip').lsp_expand(args.body)
-					-- require('snippy').expand_snippet(args.body)
-					-- vim.fn["vsnip#anonymous"](args.body)
-				end
+					luasnip.lsp_expand(args.body)
+				end,
 			},
-			sources = cmp.config.sources(
-				{
-					{ name = "nvim_lsp" },
-					-- snippet
-					{ name = "ultisnips" },
-					-- { name = 'luasnip' }
-					-- { name = 'snippy' },
-					-- { name = 'vsnip' },
-				},
-				{ { name = "buffer" } }
-			)
-		})
-		-- Use buffer source for `/`.
-		cmp.setup.cmdline('/', {
-			completion = { autocomplete = false },
+			completion = { completeopt = 'menu,menuone,noinsert' },
+
+			mapping = cmp.mapping.preset.insert {
+				['<C-n>'] = cmp.mapping.select_next_item(),
+				['<C-p>'] = cmp.mapping.select_prev_item(),
+
+				-- Scroll the documentation window [b]ack / [f]orward
+				['<C-b>'] = cmp.mapping.scroll_docs(-4),
+				['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+				['<C-y>'] = cmp.mapping.confirm { select = true },
+
+				-- Manually trigger a completion from nvim-cmp.
+				['<C-Space>'] = cmp.mapping.complete {},
+
+				-- Snippet expansion motions.
+				['<C-l>'] = cmp.mapping(function()
+					if luasnip.expand_or_locally_jumpable() then
+						luasnip.expand_or_jump()
+					end
+				end, { 'i', 's' }),
+				['<C-h>'] = cmp.mapping(function()
+					if luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
+					end
+				end, { 'i', 's' }),
+			},
 			sources = {
-				-- { name = 'buffer' }
-
-				{ name = 'buffer', opts = { keyword_pattern = [=[[^[:blank:]].*]=] } }
-			}
-		})
-
-		-- Use cmdline & path source for ':'.
-		cmp.setup.cmdline(':', {
-			completion = { autocomplete = false },
-			sources = cmp.config.sources({
-				{ name = 'path' }
-			}, {
-				{ name = 'cmdline' }
-			})
-		})
-	end
+				{ name = 'nvim_lsp' },
+				{ name = 'luasnip' },
+				{ name = 'path' },
+			},
+		}
+	end,
 }
