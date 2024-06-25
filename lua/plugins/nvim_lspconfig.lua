965bdad8be5f15ac5cb92b05b931ae11c20899f0
Arthur Zopellaro <github@zope.me>
2024-06-25 15:39:41 -0300

    feat: add ruff to lsp

--- lua/plugins/nvim_lspconfig.lua
+++ lua/plugins/nvim_lspconfig.lua
@@ -4,8 +4,7 @@ return {
 		local lsp = require("lspconfig")
 		local capabilities = require("cmp_nvim_lsp").default_capabilities() -- depends on nvim-cmp
 
-		lsp.pyright.setup({ capabilities = capabilities })
-		lsp.ruff_lsp.setup({ capabilities = capabilities })
+		lsp.ruff.setup({ capabilities = capabilities })
 		lsp.lua_ls.setup({
 			capabilities = capabilities,
 			settings = { Lua = { diagnostics = { globals = { "vim" } } } }
