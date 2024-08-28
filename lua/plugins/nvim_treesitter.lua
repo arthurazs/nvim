80f1a65594054ac1cc031d73179dc80c513f7c0d
Arthur Zopellaro <github@zope.me>
2024-08-28 12:27:53 -0300

    fix: vim-foldtext is very good, but it lacks highlighting

--- lua/plugins/nvim_treesitter.lua
+++ lua/plugins/nvim_treesitter.lua
@@ -2,7 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
     priority=999,
-    dependencies = { "jrudess/vim-foldtext" },
+    -- dependencies = { "jrudess/vim-foldtext" },
     build = ":TSUpdate",
     opts = {
         auto_install = true,
@@ -11,16 +11,17 @@ return {
     },
     config = function(_, opts)
         local opt = vim.opt
-        local g = vim.g
+        -- local g = vim.g
         require("nvim-treesitter.configs").setup(opts)
 
         -- folding
         opt.foldcolumn = "auto"
         opt.foldmethod = 'expr'
         opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        opt.foldtext = ''
         -- opt.foldnestmax = 4
         -- opt.foldlevelstart = 2
         opt.foldlevel = 99
-        g.FoldText_info = false -- vim-foldtext
+        -- g.FoldText_info = false -- vim-foldtext
     end,
 }
