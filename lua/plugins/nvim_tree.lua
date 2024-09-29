7bf74fb82b96369702d42177e07e0325a6f8ad74
Arthur Zopellaro <github@zope.me>
2024-09-29 08:50:15 -0300

    feat: hide dotfiles by default

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -6,7 +6,10 @@ return {
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
         vim.opt.termguicolors = true
-        require("nvim-tree").setup({ actions = { open_file = { quit_on_open = true } } })
+        require("nvim-tree").setup({
+            actions = { open_file = { quit_on_open = true } },
+            filters = { dotfiles = true }
+        })
     end,
     keys = {
         { "<leader>e", "<cmd>NvimTreeFindFileToggle<cr>", desc = "Toggl[e] nvim tree" }
