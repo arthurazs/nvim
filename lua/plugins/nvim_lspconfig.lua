3b2d554b48f6102922faf9b819d6dc89f419f784
Arthur Zopellaro <github@zope.me>
2025-01-01 01:03:59 -0300

    feat: add clangd lsp

--- lua/plugins/nvim_lspconfig.lua
+++ lua/plugins/nvim_lspconfig.lua
@@ -23,7 +23,7 @@ return {
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities) -- depends on nvim-cmp
 
-        local servers = { 'rust_analyzer', 'pyright', 'bashls', "ruff", "lua_ls", "gopls", "superhtml" }
+        local servers = { 'rust_analyzer', 'pyright', 'bashls', "ruff", "lua_ls", "gopls", "superhtml", "clangd" }
         for _, server in ipairs(servers) do
             local opts = {
                 handlers = handlers,
