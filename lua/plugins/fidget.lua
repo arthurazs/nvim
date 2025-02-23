59b44b62070499cff2de461235a9301f7ceb1193
Arthur Zopellaro <github@zope.me>
2025-02-22 21:04:17 -0300

    feat: update fidget

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -1,8 +1,8 @@
 return {
-    "j-hui/fidget.nvim",
-    event = "VeryLazy",
-    opts = {
-        notification = { window = { winblend = 0 } },
-        integration = { ["nvim-tree"] = { enable = false } }
-    }
+  "j-hui/fidget.nvim",
+  event = "VeryLazy",
+  opts = {
+    notification = { window = { winblend = 0 } },
+    -- integration = { ["nvim-tree"] = { enable = false } }
+  },
 }
