e635d67712b394e1541a199c1299e0fa6b4b3ae5
Arthur Zopellaro <github@zope.me>
2026-02-19 20:37:31 -0300

    wip, feat: add tab as bn/bp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,5 +4,9 @@ vim.keymap.set("n", "<Esc>", "<cmd>noh<cr>", { desc = "Clear highlights" })
 vim.keymap.set("v", "<", "<gv", { desc = "Indent visual selection" })
 vim.keymap.set("v", ">", ">gv", { desc = "Dedent visual selection" })
 
+-- buffers
+vim.keymap.set("n", "<Tab>", "<cmd>bn<cr>", { desc = "Go to next buffer" })
+vim.keymap.set("n", "<S-Tab>", "<cmd>bp<cr>", { desc = "Go to previous buffer" })
+
 -- term
 vim.keymap.set("t", "<C-x>", "<C-\\><C-n>", { desc = "[Esc]ape back to normal mode" })
